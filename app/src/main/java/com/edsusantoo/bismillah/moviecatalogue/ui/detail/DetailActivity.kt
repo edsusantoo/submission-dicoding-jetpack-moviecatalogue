0239beeda8164f8e314613cd8eaf1c91f546f5b1
Edi Susanto <edsusantoo@gmail.com>
2019-08-20 09:06:51 +0700

    viewmodel detail

--- app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/ui/detail/DetailActivity.kt
@@ -2,47 +2,50 @@ package com.edsusantoo.bismillah.moviecatalogue.ui.detail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 import com.edsusantoo.bismillah.moviecatalogue.R
-import com.edsusantoo.bismillah.moviecatalogue.data.MoviesModel
 import com.edsusantoo.bismillah.moviecatalogue.utils.Constans
 import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
+    private lateinit var detailViewModel: DetailViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
-
         if (intent != null) {
+            detailViewModel = ViewModelProviders.of(this).get(DetailViewModel::class.java)
+
             getDataIntent()
 
             setDataIntent()
         }
     }
 
-    private fun getDataIntent(): MoviesModel {
-        return intent.getParcelableExtra(Constans.EXTRAS_MOVIE)
+    private fun getDataIntent() {
+        detailViewModel.setMovieDetail(intent.getParcelableExtra(Constans.EXTRAS_MOVIE))
     }
 
     private fun setDataIntent() {
-        tv_title.text = getDataIntent().title
+        tv_title.text = detailViewModel.getMovieDetail()?.title
 
 
         var dataGenres: String? = null
-        for (i in 0 until getDataIntent().genres.size) {
+        for (i in 0 until detailViewModel.getMovieDetail()?.genres!!.size) {
             if (dataGenres == null) {
-                dataGenres = getDataIntent().genres[i]
+                dataGenres = detailViewModel.getMovieDetail()!!.genres[i]
             } else {
-                dataGenres = dataGenres + ", " + getDataIntent().genres[i]
+                dataGenres = dataGenres + ", " + detailViewModel.getMovieDetail()!!.genres[i]
             }
         }
         tv_genre.text = dataGenres
 
-        tv_rate.text = getDataIntent().rate
+        tv_rate.text = detailViewModel.getMovieDetail()?.rate
 
-        tv_description.text = getDataIntent().description
+        tv_description.text = detailViewModel.getMovieDetail()?.description
 
-        img_poster.setImageResource(getDataIntent().poster)
+        img_poster.setImageResource(detailViewModel.getMovieDetail()!!.poster)
 
     }
 }
--- app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/edsusantoo/bismillah/moviecatalogue/ui/detail/DetailViewModel.kt
@@ -1,7 +1,17 @@
 package com.edsusantoo.bismillah.moviecatalogue.ui.detail
 
 import androidx.lifecycle.ViewModel
+import com.edsusantoo.bismillah.moviecatalogue.data.MoviesModel
 
 class DetailViewModel : ViewModel() {
+    private var moviesModel: MoviesModel? = null
 
+
+    fun setMovieDetail(movies: MoviesModel) {
+        moviesModel = movies
+    }
+
+    fun getMovieDetail(): MoviesModel? {
+        return moviesModel
+    }
 }
